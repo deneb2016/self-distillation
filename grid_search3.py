d35a01be2fbaa701413d6867d32399433722c4b3
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-07-15 21:04:34 +0900

    exp session1 ready2

--- grid_search3.py
+++ grid_search3.py
@@ -7,9 +7,6 @@ D = ['1', '0.2', '0.04', '0.008']
 seed = 1
 save_dir = '../repo/distill/%s/resnet34sd/session1/' % dataset
 
-cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 1 --temp 0 --distill 0' % (GPU, seed, dataset, save_dir)
-os.system(cmd)
-
 cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp 1 --distill 0' % (GPU, seed, dataset, save_dir)
 os.system(cmd)
 
