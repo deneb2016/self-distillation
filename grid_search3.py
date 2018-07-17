18c7392812fad2e555871ebab21887e22531d0fb
deneb2016 <deneb2016@gmail.com>
2018-07-18 00:48:10 +0900

    session2

--- grid_search3.py
+++ grid_search3.py
@@ -1,16 +1,31 @@
+# import os
+#
+# GPU = 3
+# dataset = 'cifar100'
+# T = ['9', '11', '13', '15']
+# D = ['1', '0.2', '0.04', '0.008']
+# seed = 1
+# save_dir = '../repo/distill/%s/resnet34sd/session1/' % dataset
+#
+# cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp 1 --distill 0' % (GPU, seed, dataset, save_dir)
+# os.system(cmd)
+#
+# for t in T:
+#     for d in D:
+#         cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp %s --distill %s' % (GPU, seed, dataset, save_dir, t, d)
+#         os.system(cmd)
+
 import os
 
 GPU = 3
 dataset = 'cifar100'
-T = ['9', '11', '13', '15']
-D = ['1', '0.2', '0.04', '0.008']
+FROM = ['31', '101']
+D = ['1', '0.2', '0.04']
 seed = 1
-save_dir = '../repo/distill/%s/resnet34sd/session1/' % dataset
+save_dir = '../repo/distill/%s/resnet34sd/session2/' % dataset
 
-cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp 1 --distill 0' % (GPU, seed, dataset, save_dir)
-os.system(cmd)
-
-for t in T:
+for df in FROM:
     for d in D:
-        cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp %s --distill %s' % (GPU, seed, dataset, save_dir, t, d)
+        cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session2.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp 9 --distill_from %s --distill %s' % (GPU, seed, dataset, save_dir, df, d)
         os.system(cmd)
+
