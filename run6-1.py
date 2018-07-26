fc6af7f7adce7e23a33aa574829e90b31744d672
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-07-26 17:06:48 +0900

    run6

---
+++ run6-1.py
@@ -0,0 +1,15 @@
+# session1 CIFIAR10 part2 seed5
+
+import os
+
+GPU = 1
+dataset = 'cifar10'
+T = ['3', '11']
+D = ['1', '0.2', '0.04', '0.008']
+seed = 5
+save_dir = '../repo/distill/%s/resnet34sd/session1/' % dataset
+
+for t in T:
+    for d in D:
+        cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp %s --distill %s' % (GPU, seed, dataset, save_dir, t, d)
+        os.system(cmd)
