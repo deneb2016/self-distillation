26271d3871cba265ff91a61b73a74e5756fbe525
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-07-27 15:55:41 +0900

    run3-3 recover

--- run3-3.py
+++ run3-3.py
@@ -4,15 +4,12 @@ import os
 
 GPU = 3
 dataset = 'cifar100'
-# T = ['7', '15']
-# D = ['1', '0.2', '0.04', '0.008']
+T = ['7', '15']
+D = ['1', '0.2', '0.04', '0.008']
 seed = 4
 save_dir = '../repo/distill/%s/resnet34sd/session1/' % dataset
-#
-# for t in T:
-#     for d in D:
-#         cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp %s --distill %s' % (GPU, seed, dataset, save_dir, t, d)
-#         os.system(cmd)
 
-cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 1 --temp 0 --distill 0' % (GPU, seed, dataset, save_dir)
-os.system(cmd)
+for t in T:
+    for d in D:
+        cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session1.py --seed %d --dataset %s --save_dir %s --stoch_depth 0.5 --temp %s --distill %s' % (GPU, seed, dataset, save_dir, t, d)
+        os.system(cmd)
