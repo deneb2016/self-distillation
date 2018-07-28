c169d26525eee31fd284d7b645f16f97458f6baa
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-07-29 02:50:49 +0900

    remove savedir arg

--- run11-0.py
+++ run11-0.py
@@ -3,7 +3,6 @@ import os
 GPU = 0
 dataset = 'cifar10'
 SEED = [1, 2, 3, 4, 5]
-save_dir = '../repo/distill/%s/vgg16do/session4/' % dataset
 for seed in SEED:
-    cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session4.py --seed %d --dataset %s --save_dir %s --drop_p 0.3 --feat_dim 2048 --wd 0' % (GPU, seed, dataset, save_dir)
+    cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session4.py --seed %d --dataset %s --drop_p 0.3 --feat_dim 2048 --wd 0' % (GPU, seed, dataset)
     os.system(cmd)
