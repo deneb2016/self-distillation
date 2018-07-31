b465246f50e96995e498d2eb7c5bcb561b5a254f
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-07-31 14:23:11 +0900

    ready for session4 wd0.00001

--- run3-2.py
+++ run3-2.py
@@ -4,5 +4,5 @@ GPU = 2
 dataset = 'cifar100'
 SEED = [1, 2, 3, 4, 5]
 for seed in SEED:
-    cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session4.py --seed %d --dataset %s --drop_p 0.3 --feat_dim 2048 --wd 0.0001' % (GPU, seed, dataset)
+    cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session4.py --seed %d --dataset %s --drop_p 0.5 --feat_dim 2048 --wd 0.00001' % (GPU, seed, dataset)
     os.system(cmd)
--- run6-1.py
+++
@@ -1,8 +0,0 @@
-import os
-
-GPU = 1
-dataset = 'cifar100'
-SEED = [1, 2, 3, 4, 5]
-for seed in SEED:
-    cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session4.py --seed %d --dataset %s --drop_p 0.3 --feat_dim 4096 --wd 0' % (GPU, seed, dataset)
-    os.system(cmd)
