e8664a76ee354f43e423028b1f3266cae895488c
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-08-16 15:48:46 +0900

    ready for session10 various drop rate

--- run4-0.py
+++ run4-0.py
@@ -5,5 +5,5 @@ dataset = 'cifar100'
 seed = 1
 drop_p = '0.5'
 
-cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session11.py --seed %d --dataset %s --drop_p %s --no_aug' % (GPU, seed, dataset, drop_p)
+cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session10.py --seed %d --dataset %s --drop_p %s --no_aug' % (GPU, seed, dataset, drop_p)
 os.system(cmd)
--- run6-0.py
+++ run6-0.py
@@ -5,5 +5,5 @@ dataset = 'cifar10'
 seed = 1
 drop_p = '0.4'
 
-cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session11.py --seed %d --dataset %s --drop_p %s --no_aug' % (GPU, seed, dataset, drop_p)
+cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session10.py --seed %d --dataset %s --drop_p %s --no_aug' % (GPU, seed, dataset, drop_p)
 os.system(cmd)
