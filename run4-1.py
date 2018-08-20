8b8481d13ae579d3db6c417ffe9a2535103b38de
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-08-20 17:25:39 +0900

    aaa

--- run4-1.py
+++ run4-1.py
@@ -1,10 +1,10 @@
 import os
 
 GPU = 1
-dataset = 'cifar10'
+dataset = 'cifar100'
 SEED = [1, 2, 3, 4, 5]
-drop_p = '0.6'
+drop_p = '0.8'
 
 for seed in SEED:
-    cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session4.py --seed %d --dataset %s --drop_p %s' % (GPU, seed, dataset, drop_p)
+    cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session4.py --seed %d --dataset %s --drop_p %s --drop_last_only' % (GPU, seed, dataset, drop_p)
     os.system(cmd)
