56bd24aadfdaa992689d9263d16cd9f02aa18ece
이승관(Seungkwan Lee) <seungkwan@postech.ac.kr>
2018-08-25 17:01:31 +0900

    aa

--- run6-2.py
+++ run6-2.py
@@ -2,12 +2,12 @@ import os
 
 GPU = 2
 dataset = 'cifar10'
-SEED = [4, 5]
+SEED = [1, 2, 3, 4, 5]
 
 D = ['0.1', '0.02', '0.004']
 t = '5'
 wd = '0.0001'
 for seed in SEED:
     for d in D:
-        cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session12.py --seed %d --dataset %s --drop_last_only --wd %s --temp %s --distill %s' % (GPU, seed, dataset, wd, t, d)
+        cmd = 'CUDA_VISIBLE_DEVICES=%d python train_eval_session13.py --seed %d --dataset %s --wd %s --temp %s --distill %s' % (GPU, seed, dataset, wd, t, d)
         os.system(cmd)
